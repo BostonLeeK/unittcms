6e48da612c2a6eb7419adb485d75334e62673f9b
Oleksii Sysoiev <bostonleek@gmail.com>
2026-01-28 16:52:41 +0200

    feat: increase JSON payload limit in server middleware to 100mb for handling larger requests

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // enable json middleware
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 
 // enable rate limiter
 const limiter = RateLimit({
